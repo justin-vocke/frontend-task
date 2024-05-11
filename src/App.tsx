718f43f2d5d24536b737fcf50d029dbf94197ad0
Justin Vocke <therealmathhelper@gmail.com>
2024-05-10 20:16:47 -0500

    add appropriate identity/graph call functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,81 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React, { useState } from "react";
 import "./App.css";
+import Button from "react-bootstrap/Button";
+import { PageLayout } from "./components/PageLayout";
+import { loginRequest } from "./authConfig";
+import { callMsGraph } from "./graph";
+import { ProfileData } from "./components/ProfileData";
+
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+  useMsal,
+} from "@azure/msal-react";
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  /**
+   * Renders information about the signed-in user or a button to retrieve data about the user
+   */
+  const ProfileContent = () => {
+    const { instance, accounts } = useMsal();
+    const [graphData, setGraphData] = useState(null);
 
+    function RequestProfileData() {
+      // Silently acquires an access token which is then attached to a request for MS Graph data
+      instance
+        .acquireTokenSilent({
+          ...loginRequest,
+          account: accounts[0],
+        })
+        .then((response) => {
+          callMsGraph(response.accessToken).then((response) =>
+            setGraphData(response)
+          );
+        });
+    }
+
+    return (
+      <>
+        <h5 className="card-title">Welcome {accounts[0].name}</h5>
+        <br />
+        {graphData ? (
+          <ProfileData graphData={graphData} />
+        ) : (
+          <Button variant="secondary" onClick={RequestProfileData}>
+            Request Profile Information
+          </Button>
+        )}
+      </>
+    );
+  };
+
+  /**
+   * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
+   */
+  const MainContent = () => {
+    return (
+      <div className="App">
+        <AuthenticatedTemplate>
+          <ProfileContent />
+        </AuthenticatedTemplate>
+
+        <UnauthenticatedTemplate>
+          <h5>
+            <center>Please sign-in to see your profile information.</center>
+          </h5>
+        </UnauthenticatedTemplate>
+      </div>
+    );
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PageLayout>
+        <center>
+          <MainContent />
+        </center>
+      </PageLayout>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { PublicClientApplication } from "@azure/msal-browser";
+import { MsalProvider } from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const msalInstance = new PublicClientApplication(msalConfig);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>
+);
